bf1cc66e37f4db4444b3cc1d4f8d389a513b0e19
allenduo <18717860675@163.com>
2016-09-01 21:56:05 +0800

    show a Image

--- LearningOpenCV/LearningOpenCV.cpp
+++ LearningOpenCV/LearningOpenCV.cpp
@@ -2,10 +2,15 @@
 //
 
 #include "stdafx.h"
+#include <opencv2\opencv.hpp>
 
-
+using namespace cv;
 int _tmain(int argc, _TCHAR* argv[])
 {
+	Mat srcImage = imread("E:\\openCV document\\picture\\1.jpg");
+	namedWindow("Example", CV_WINDOW_AUTOSIZE);
+	imshow("Example", srcImage);
+	waitKey(0);
 	return 0;
 }
 
